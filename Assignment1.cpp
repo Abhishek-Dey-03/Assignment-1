e938b1c219066de48c76c02da3757a51ee663945
Abhishek-Dey-03 <abhishek.dey@student.manchester.ac.uk>
2024-01-31 10:31:42 +0000

    This adds some of the comments at the start to indicate what this program does

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -1,1 +1,3 @@
-//empty cpp file
+//Assignment 1 10821797
+//This program is created to use the simple Bohr formula to model electron transitions.
+//This program will take user input Atomic Numeber Z and 
