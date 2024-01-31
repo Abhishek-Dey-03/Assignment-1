b9811610d66f1743888f3d2ac132d529a743d3b3
Abhishek-Dey-03 <abhishek.dey@student.manchester.ac.uk>
2024-01-31 14:38:21 +0000

    ISSUE: the false input check prints twice

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -11,21 +11,21 @@ It will output via user request either Joules or eV.
 #include <iostream>
 #include <iomanip>
 
-double energyCalc(int Z, int n_i, int n_f, char energy_choice) 
+double energy_calc(int Z, int n_i, int n_f, char energy_choice) 
 {
     double reciprocal_initial= 1.0/(n_i*n_i);
     double reciprocal_final= 1.0/(n_f*n_f);
-    double e;
+    double E; //energy
 
     if(energy_choice == 'e') 
     {
-         e = 13.6 * Z * Z * (reciprocal_final - reciprocal_initial);
+         E = 13.6 * Z * Z * (reciprocal_final - reciprocal_initial);
     }
     else 
     {
-        e = 13.6 * Z * Z * (reciprocal_final - reciprocal_initial) * 1.60218e-19;
+        E = 13.6 * Z * Z * (reciprocal_final - reciprocal_initial) * 1.60218e-19;
     }
-    return e;
+    return E;
 }
 
 int main() 
@@ -40,14 +40,14 @@ int main()
         char energy_choice; //choice of energy
         
     // take in input for atomic number
-        std::cout<<"Enter the Atomic Number, intial quantum number, final quantum number and your choice of energy. The energy choice should be e (eV) or J (Joules): ";
+        std::cout<<"Enter the Atomic Number, initial quantum number, final quantum number and your choice of energy. The energy choice should be e (eV) or J (Joules): ";
         std::cin>>Z>>n_i>>n_f>>energy_choice;
 
     // catch erroneous input
-        while(std::cin.fail()) 
+        while(!isdigit(Z)||!isdigit(n_i)||!isdigit(n_f)) 
         {
-            std::cout<<"There is an invalid input. Please try again:";
-            std::cout<<"Enter the Atomic Number, intial quantum number, final quantum number and your choice of energy. The energy choice should be e (eV) or J (Joules): "; 
+            std::cout<<"There is an invalid input. Please try again:\n";
+            std::cout<<"Enter the Atomic Number, initial quantum number, final quantum number and your choice of energy. The energy choice should be e (eV) or J (Joules): "; 
             //ignore any non-integer inputs, clear the variable and ignore input.
             std::cin.clear();
             std::cin.ignore();
@@ -58,17 +58,32 @@ int main()
 
         }
 
-        while(energy_choice != 'e' && energy_choice != 'J') 
+        while(n_i<n_f)  
         {
-            std::cout << "Invalid choice. Please enter 'e' for energy in electron volts, or 'J' for energy in joules: ";
-            std::cout<<"Enter the Atomic Number, intial quantum number, final quantum number and your choice of energy. The energy choice should be e (eV) or J (Joules): ";
+            std::cout<<"Invalid choice. Please ensure the final stage is less than the initial stage:\n ";
+            std::cout<<"Enter the Atomic Number, initial quantum number, final quantum number and your choice of energy. The energy choice should be e (eV) or J (Joules): ";
+            std::cin.clear();
+            std::cin.ignore();
             std::cin>>Z;
             std::cin>>n_i;
             std::cin>>n_f;
             std::cin>>energy_choice;
         }
 
-        double energy = energyCalc(Z, n_i, n_f, energy_choice);
+
+        while(energy_choice != 'e' && energy_choice != 'J') 
+        {
+            std::cout<<"Invalid choice. Please enter 'e' for energy in electron volts, or 'J' for energy in joules:\n ";
+            std::cout<<"Enter the Atomic Number, initial quantum number, final quantum number and your choice of energy. The energy choice should be e (eV) or J (Joules): ";
+            std::cin.clear();
+            std::cin.ignore();
+            std::cin>>Z;
+            std::cin>>n_i;
+            std::cin>>n_f;
+            std::cin>>energy_choice;
+        }
+
+        double energy = energy_calc(Z, n_i, n_f, energy_choice); //assign energy from function output
         if(energy_choice == 'e') 
         {
             std::cout<<"Transition Energy: "<<energy<<"eV."<<std::endl;
@@ -85,6 +100,8 @@ int main()
         while(repeat_choice != 'y' && repeat_choice != 'n') 
         {
             std::cout<<"Invalid choice. Please enter 'y' to repeat or 'n' to exit: ";
+            std::cin.clear();
+            std::cin.ignore();
             std::cin>>repeat_choice;
         }
     
@@ -93,11 +110,3 @@ int main()
 
     return 0;
 }
-
-    
-
-
-
-
-
-
